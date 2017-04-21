043506ea6fc3baae91fb6af7469a3bd803275092
Andy Illarionov <xeniuss@iMac-andrew.local>
2017-04-22 03:33:47 +0400

    bugfix

--- js/app.js
+++ js/app.js
@@ -6,3 +6,8 @@ $('#parallax').parallax({
     frictionY: .1
 });
 
+$('.scroller').click(function () {
+    var scrollId = '#' + $(this).attr('data-scrollto');
+    $("html, body").animate({ scrollTop: $(scrollId).offset().top }, 1000);
+
+})
